29d2e7c7167e2fd90cff603f6fa3ea6eb253e581
Joseph Hsu <jhsu@josephhsu.com>
2010-07-28 15:32:41 -0400

    add mountable rack app

---
+++ lib/redis-shelf/mount.rb
@@ -0,0 +1,19 @@
+class RedisShelved
+	require './server'
+
+	def initialize(app)
+		@app = app
+	end
+
+	def call(env)
+		if env["PATH_INFO"] =~ /^\/redis/
+			env["PATH_INFO"].gsub!(/^\/redis/, '')
+			env['SCRIPT_NAME'] = ''
+			env['REQUEST_URI'].gsub!(/^\/redis/, '')
+			app = Rack::RedisShelfServer.new(:host => "localhost", :db => 7)
+			app.call(env)
+		else
+			@app.call(env)
+		end
+	end
+end
