9d067e0df6ac754e482f23fe6cceeaba11a15799
Joseph Hsu <jhsu@josephhsu.com>
2011-02-15 11:37:23 -0500

    fix view and use jquery mobile

--- config.ru
+++ config.ru
@@ -2,8 +2,9 @@
 
 $LOAD_PATH.unshift ::File.expand_path(::File.dirname(__FILE__) + '/lib')
 require 'redis-shelf/server'
+require 'yaml'
 
 use Rack::Static, :urls => ["/favicon"]
 use Rack::Static, :urls => ["/css"], :root => "public"
-app = Rack::RedisShelfServer.new(:host => "localhost", :port => 6379, :db => 7)
+app = Rack::RedisShelfServer.new(YAML.load_file('./config/redis.yml'))
 run app
--- lib/rack/redis-shelf.rb
+++ lib/rack/redis-shelf.rb
@@ -1,47 +1,47 @@
 require 'redis'
 
 module Rack
-	class RedisShelf
-		F = ::File
+  class RedisShelf
+    F = ::File
 
-		attr_accessor :config
+    attr_accessor :config
 
-		def initialize(conf={})
-		  self.config = {:host => "localhost", :port => 6379, :db => 0}.merge(conf)
-		end
+    def initialize(conf={})
+      self.config = {'host' => "localhost", 'port' => 6379, 'db' => 1}.merge(conf)
+    end
 
-		def connection
-			::Redis.new(self.config)
-		end
+    def connection
+      Thread.current[:redis] ||= ::Redis.new(self.config)
+    end
 
-		def parse(key)
-			if key.empty?
-				""
-			else
-				key.gsub(/^\/|\/$/,'').gsub(/[\s]+/,'').gsub('/',':')
-			end
-		end
+    def parse(key)
+      if key.empty?
+        ""
+      else
+        key.gsub(/^\/|\/$/,'').gsub(/[\s]+/,'').gsub('/',':')
+      end
+    end
 
-		def fetch(key)
-			key = parse(key)
-			if !key.empty?
-				case self.connection.type(key)
-				when "string" || "none"
-					connection.get(key) || ""
-				when "list"
-					connection.lrange(key, 0, -1)
-				else
-					""
-				end
-			else
-				""
-			end
-		end
+    def fetch(key)
+      key = parse(key)
+      if !key.empty?
+        case self.connection.type(key)
+        when "string" || "none"
+          connection.get(key) || ""
+        when "list"
+          connection.lrange(key, 0, -1)
+        else
+          ""
+        end
+      else
+        ""
+      end
+    end
 
-		def http_response(key)
-			value = fetch(key)
-			code = value ? 200 : 404
-			[value, code]
-		end
-	end
+    def http_response(key)
+      value = fetch(key)
+      code = value ? 200 : 404
+      [value, code]
+    end
+  end
 end
